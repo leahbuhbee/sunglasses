bf5e698fc7b503aa1fb272d3504beee69b08071b
leahbuhbee <leahbuhbee@gmail.com>
2025-03-03 15:13:03 +1100

    Initial commit with icon

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# sunglasses
+<!--suppress HtmlDeprecatedTag, XmlDeprecatedElement -->
+<center><img alt="screenshot" src="" /></center>
+
+<center>
+Adds wearable sunglasses to the game.
+</center>
+
+---
+
+---
+
+### Afterword
+
+
+---
+
---
+++ src/main/java/io/github/leahbuhbee/Sunglasses.java
@@ -0,0 +1,25 @@
+package io.github.leahbuhbee;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.lifecycle.v1.CommonLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sunglasses implements ModInitializer {
+	public static final String ID = "sunglasses";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+
+	@Override
+	public void onInitialize() {
+        LOGGER.info("[Sunglasses] Barks at your sun");
+	}
+}
