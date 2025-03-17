0485e09805d6320fb3068d6585e9d1e0a3fb1526
leahbuhbee <leahbuhbee@gmail.com>
2025-03-17 17:59:29 +1100

    punch equip on mobs

--- src/main/java/io/github/leahbuhbee/item/SunglassesItem.java
+++ src/main/java/io/github/leahbuhbee/item/SunglassesItem.java
@@ -1,6 +1,7 @@
 package io.github.leahbuhbee.item;
 
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Equipment;
 import net.minecraft.item.Item;
@@ -23,5 +24,15 @@ public class SunglassesItem extends Item implements Equipment {
 	@Override
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 		return this.equipAndSwap(this, world, user, hand);
+	}
+
+	@Override
+	public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+		EquipmentSlot equipmentSlot = target.getPreferredEquipmentSlot(stack);
+		if (target.getEquippedStack(equipmentSlot).isEmpty() ) {
+			target.equipStack(getSlotType(),stack.copyWithCount(1));
+			stack.decrement(1);
+		}
+		return super.postHit(stack, target, attacker);
 	}
 }
