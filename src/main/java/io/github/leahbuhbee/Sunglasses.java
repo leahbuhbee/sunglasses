0b53e6ec4303dd22b082c8234422e77271aa2bf0
leahbuhbee <leahbuhbee@gmail.com>
2025-03-17 18:20:02 +1100

    Added to creative menu (tools & utilities)

--- src/main/java/io/github/leahbuhbee/Sunglasses.java
+++ src/main/java/io/github/leahbuhbee/Sunglasses.java
@@ -1,19 +1,14 @@
 package io.github.leahbuhbee;
 
 import io.github.leahbuhbee.item.SunglassesItem;
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.event.lifecycle.v1.CommonLifecycleEvents;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +19,8 @@ public class Sunglasses implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.ITEM, Identifier.of(Sunglasses.ID, "sunglasses"), new SunglassesItem(new Item.Settings()));
-        LOGGER.info("[Sunglasses] Barks at your sun");
+		Item sunglasses = Registry.register(Registries.ITEM, Identifier.of(Sunglasses.ID, "sunglasses"), new SunglassesItem(new Item.Settings()));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> entries.addBefore(Items.ELYTRA, sunglasses));
+		LOGGER.info("[Sunglasses] Barks at your sun");
 	}
 }
